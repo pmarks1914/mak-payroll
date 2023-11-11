2247b91024964520bc7a0ceb5298617cba5bc5fb
Patrick Marks <pmarks1914@gmail.com>
2023-11-11 13:03:04 +0000

    detail page restructured

--- src/views/pages/Salary/SalaryDetail.js
+++ src/views/pages/Salary/SalaryDetail.js
@@ -1,7 +1,22 @@
 import React from 'react';
 import { Counter } from './Counter';
 import Box from '@mui/material/Box';
-import { CBadge } from '@coreui/react';
+import {
+    CBadge,
+    CCard,
+    CCardBody,
+    CCardFooter,
+    CCardGroup,
+    CCardHeader,
+    CCardImage,
+    CCardLink,
+    CCardSubtitle,
+    CCardText,
+    CCardTitle,
+    CButton,
+    CListGroup,
+    CListGroupItem
+} from '@coreui/react';
 import HomeIcon from '@mui/icons-material/Home';
 import moment from 'moment';
 import { Col, Row } from 'reactstrap';
@@ -9,133 +24,166 @@ import { Col, Row } from 'reactstrap';
 
 const SalaryDetail = () => {
     return (
-        <div className='d-flex justify-content-center' >
+        <div className='d-flex justify-content-center' style={{ margin: '0px 20px 0px 0px' }}  >
             {/* <Counter /> */}
 
-            <Box style={{ width: '60%', margin: '0px 3px' }} >
+            {/* <CCard>
+                <CCardHeader></CCardHeader>
+                <CCardBody>
+                  <CCardTitle></CCardTitle>
+                  <CCardText>
+                  </CCardText>
+                  
+                </CCardBody>
+              </CCard> */}
+
+            <Box style={{ width: '50%', margin: '0px 0px' }} >
                 {/* <Grid container rowSpacing={1} columnSpacing={{ xs: 1, sm: 2, md: 3 }}> */}
+                <CCard className='mt-5'>
+                    <p className='d-flex justify-content-center mt-4 mb-2 '> <h4>{("Details").toUpperCase() }</h4> </p>
+                    <CListGroup flush>
 
-                <Row className='mt-4 mb-0 d-flex'>
-                    <Col sm="4" xm="4" md="4" lg="4" xl="4">
-                        <a style={{ color: "#000" }}>
-                            <p className=""> User  </p>
-                        </a>
-                    </Col>
-                    <Col sm="4"></Col>
-                    <Col sm="4">
-                        <a style={{ color: "#000" }}>
+                        <CListGroupItem>
 
-                        </a>
-                    </Col>
-                    <hr />
-                </Row>
-                <Row className='mb-0 d-flex'>
-                    <Col sm="4" xm="4" md="4" lg="4" xl="4">
-                        <a style={{ color: "#000" }}>
-                            <p className="">Name</p>
-                        </a>
-                    </Col>
-                    <Col sm="4"></Col>
-                    <Col sm="4">
-                        <a style={{ color: "#000" }}>
+                            <Row className='mb-0 d-flex'>
+                                <Col sm="4" xm="4" md="4" lg="4" xl="4">
+                                    <a style={{ color: "#000" }}>
+                                        <p className="">Name</p>
+                                    </a>
+                                </Col>
+                                <Col sm="4"></Col>
+                                <Col sm="4">
+                                    <a style={{ color: "#000" }}>
+                                        jj
+                                    </a>
+                                </Col>
 
-                        </a>
-                    </Col>
-                    <hr />
-                </Row>
-                <Row className='mb-0 d-flex'>
-                    <Col sm="4" xm="4" md="4" lg="4" xl="4">
-                        <a style={{ color: "#000" }}>
-                            <p className=""> Number</p>
-                        </a>
-                    </Col>
-                    <Col sm="4"></Col>
-                    <Col sm="4">
-                        <a style={{ color: "#000" }}>
+                            </Row>
 
-                        </a>
-                    </Col>
-                    <hr />
-                </Row>
+                        </CListGroupItem>
 
-                <Row className='mb-0 d-flex'>
-                    <Col sm="4" xm="4" md="4" lg="4" xl="4">
-                        <a style={{ color: "#000" }}>
-                            <p className=""> Type</p>
-                        </a>
-                    </Col>
-                    <Col sm="4"></Col>
-                    <Col sm="4">
-                        <a style={{ color: "#000" }}>
+                        <CListGroupItem>
 
-                        </a>
-                    </Col>
-                    <hr />
-                </Row>
-                <Row className='mb-0 d-flex'>
-                    <Col sm="4" xm="4" md="4" lg="4" xl="4">
-                        <a style={{ color: "#000" }}>
-                            <p className=""> Message </p>
-                        </a>
-                    </Col>
-                    <Col sm="4"></Col>
-                    <Col sm="4">
-                        <a style={{ color: "#000" }}>
+                            <Row className='mb-0 d-flex'>
+                                <Col sm="4" xm="4" md="4" lg="4" xl="4">
+                                    <a style={{ color: "#000" }}>
+                                        <p className=""> Number</p>
+                                    </a>
+                                </Col>
+                                <Col sm="4"></Col>
+                                <Col sm="4">
+                                    <a style={{ color: "#000" }}>
 
-                        </a>
-                    </Col>
-                    <hr />
-                </Row>
-                <Row className='mb-0 d-flex'>
-                    <Col sm="4" xm="4" md="4" lg="4" xl="4">
-                        <a style={{ color: "#000" }}>
-                            <p className=""> Status </p>
-                        </a>
-                    </Col>
-                    <Col sm="4"></Col>
-                    <Col sm="4">
-                        <a style={{ color: "#000" }}>
-                            <CBadge color="" >
-                                hh
-                            </CBadge>
+                                    </a>
+                                </Col>
 
-                        </a>
-                    </Col>
-                    <hr />
-                </Row>
-                <Row className='mb-0 d-flex'>
-                    <Col sm="4" xm="4" md="4" lg="4" xl="4">
-                        <a style={{ color: "#000" }}>
-                            <p className=""> Date </p>
-                        </a>
-                    </Col>
-                    <Col sm="4"></Col>
-                    <Col sm="4">
-                        <a style={{ color: "#000" }}>
-                            hh
-                            {/* {moment(post?.viewData?.created_at).format('LLLL')} */}
-                        </a>
-                    </Col>
-                    <hr />
-                </Row>
-                <Row className='mb-0 d-flex'>
-                    <Col sm="4" xm="2" md="2" lg="2" xl="2">
-                        <a style={{ color: "#000" }}>
-                            {/* <p className=""> Device Info. </p> */}
-                        </a>
-                    </Col>
-                    <Col sm="8" className='ml-120'>
-                        <p style={{ color: "#000", textAlign: "center" }} >
-                            <b> More. </b> <br />
+                            </Row>
 
-                        </p>
+                        </CListGroupItem>
 
-                    </Col>
-                    <Col sm="2">
+                        <CListGroupItem>
 
-                    </Col>
-                    {/* <hr /> */}
-                </Row>
+                            <Row className='mb-0 d-flex'>
+                                <Col sm="4" xm="4" md="4" lg="4" xl="4">
+                                    <a style={{ color: "#000" }}>
+                                        <p className=""> Type</p>
+                                    </a>
+                                </Col>
+                                <Col sm="4"></Col>
+                                <Col sm="4">
+                                    <a style={{ color: "#000" }}>
+
+                                    </a>
+                                </Col>
+
+                            </Row>
+
+                        </CListGroupItem>
+
+                        <CListGroupItem>
+
+                            <Row className='mb-0 d-flex'>
+                                <Col sm="4" xm="4" md="4" lg="4" xl="4">
+                                    <a style={{ color: "#000" }}>
+                                        <p className=""> Message </p>
+                                    </a>
+                                </Col>
+                                <Col sm="4"></Col>
+                                <Col sm="4">
+                                    <a style={{ color: "#000" }}>
+
+                                    </a>
+                                </Col>
+
+                            </Row>
+
+                        </CListGroupItem>
+
+                        <CListGroupItem>
+
+                            <Row className='mb-0 d-flex'>
+                                <Col sm="4" xm="4" md="4" lg="4" xl="4">
+                                    <a style={{ color: "#000" }}>
+                                        <p className=""> Status </p>
+                                    </a>
+                                </Col>
+                                <Col sm="4"></Col>
+                                <Col sm="4">
+                                    <a style={{ color: "#000" }}>
+                                        <CBadge color="" >
+                                            hh
+                                        </CBadge>
+
+                                    </a>
+                                </Col>
+
+                            </Row>
+                        </CListGroupItem>
+
+                        <CListGroupItem>
+
+                            <Row className='mb-0 d-flex'>
+                                <Col sm="4" xm="4" md="4" lg="4" xl="4">
+                                    <a style={{ color: "#000" }}>
+                                        <p className=""> Date </p>
+                                    </a>
+                                </Col>
+                                <Col sm="4"></Col>
+                                <Col sm="4">
+                                    <a style={{ color: "#000" }}>
+                                        hh
+                                        {/* {moment(post?.viewData?.created_at).format('LLLL')} */}
+                                    </a>
+                                </Col>
+
+                            </Row>
+                        </CListGroupItem>
+
+                        <CListGroupItem>
+
+                            <Row className='mb-0 d-flex'>
+                                <Col sm="4" xm="2" md="2" lg="2" xl="2">
+                                    <a style={{ color: "#000" }}>
+                                        {/* <p className=""> Device Info. </p> */}
+                                    </a>
+                                </Col>
+                                <Col sm="8" className='ml-120'>
+                                    <p style={{ color: "#000", textAlign: "center" }} >
+                                        <b> More. </b> <br />
+
+                                    </p>
+
+                                </Col>
+                                <Col sm="2">
+
+                                </Col>
+
+                            </Row>
+                        </CListGroupItem>
+                        
+                    </CListGroup>
+
+                </CCard>
 
 
             </Box>
